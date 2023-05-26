2adcf5423e2b383b286dcbb0d5049b5816eacc70
MrChan <mrkcwayne@gmail.com>
2023-05-25 19:42:05 -0600

    coomit

---
+++ Light house notes/Primitive.js
@@ -0,0 +1,7 @@
+// Let's review the six types of primitives:
+// -undefined : is a non-configurable, non-writable property [eto yung result, wala kang say kung bakit sya undefined resulta lang sya]
+// -null :  intentional absence of any object value [ikaw yung nag bigay ng value na wala syang value para bang 0 = null pero ang null is parang void]
+// -boolean : represents a truth value: true or false. [ang boolean ay iba sa [true] at [false] sya ay parang questionaire na may dalawang opsiyon [true] or [false]]
+// -string : represent and manipulate a sequence of characters. [end of the day sya ay linya ng mga salita or words]
+// -number : represent floating-point numbers like 37 or -9.25. [numero]
+
