ea7351e3d3290a5227fc3ba9ba7c1cc58d4f4b48
MrChan <mrkcwayne@gmail.com>
2023-05-30 17:16:58 -0600

    sup not run

--- Code Thinkering/Test file 3.js
+++ Code Thinkering/Test file 3.js
@@ -5,3 +5,13 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const eqObjects = function(object1, object2) {
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+eqObjects(shirtObject , anotherShirtObject); // => true
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject , longSleeveShirtObject); // => false
