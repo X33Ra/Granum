42b16d11f17bb05d833e81126f3acb9d0ed1581f
MrChan <mrkcwayne@gmail.com>
2023-06-14 01:23:34 -0600

    coomitus

---
+++ Code Thinkering/Test file 24.js
@@ -0,0 +1,20 @@
+const args = process.argv.slice(2);
+
+const scheduleAlarm = function(time) {
+  if (time <= 0 || isNaN(time)) {
+    return;
+  }
+  
+  setTimeout(() => {
+    console.log('Beep!');
+    process.stdout.write('\x07'); // Beep sound
+  }, time * 1000);
+};
+
+for (const arg of args) {
+  const time = parseInt(arg, 10);
+  scheduleAlarm(time);
+  if (!isNaN(time) && time > 0) {
+    console.log(`Alarm scheduled in ${time} seconds`);
+  }
+}
