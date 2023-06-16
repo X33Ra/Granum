281f4f24862edf8a12f1b9bc6e6f29ce1fda8833
MrChan <mrkcwayne@gmail.com>
2023-06-16 11:08:00 -0600

    test file why u here?

---
+++ Code Thinkering/Test file 25.js
@@ -0,0 +1,11 @@
+// Available move commands
+// const moves = ["Move: up", "Move: down", "Move: left", "Move: right"];
+
+// // Index to track the current move command
+// let moveIndex = 0;
+
+// // Move the snake in all four directions every 50ms
+//
+//
+//
+//
